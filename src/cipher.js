0788e49ad0efb93c189c6609eec25d188e2e3474
Drancer55 <antonio281255@gmail.com>
2021-10-07 23:13:07 -0500

    Arreglo para codificar y decodificar correctamente carácteres especiales

--- src/cipher.js
+++ src/cipher.js
@@ -11,12 +11,14 @@
 //De acuerdo con el número que abarcan los códigos del abecedario en American Standard Code for Information Interchange (ASCII) buscaremos los parámetros (en este caso de 32 al 64 para números y carácteres como "%&/())
 //Cada console.log muestra que el código pasa y lo "lee" imprimiendolo en la consola del Browser
       if (cifrar[i].charCodeAt() >= 33 && cifrar[i].charCodeAt() <= 64) {
-        codeNumber = (cifrar[i]);
+        let simbol = (cifrar[i].charCodeAt() - 33 + offset) % 32 + 33;
+        let codeNumber = String.fromCharCode(simbol);
         cifrado += codeNumber;
           console.log("Números y/o simbolos codificados")
 //los carácteres que se abarcan en este bloque corresponden a letras con acentos y carácteres especiales (del 123 al 254 ASCII) por ejemplo: ñ{á}éçó
       } else if (cifrar[i].charCodeAt() >= 123 && cifrar[i].charCodeAt() <= 254) {
-        codeNumber = (cipher[i]);
+        let character = (cifrar[i].charCodeAt() - 123 + offset) % 132 + 123; //132 carácteres en este bloque
+        let codeNumber = String.fromCharCode(character);
         cifrado += codeNumber;
         console.log("Carácter especial codificado")
 //32 ASCII corresponde al carácter "space"
@@ -26,7 +28,7 @@
         console.log("Espacio codificado");
 //65 al 90 ASCII corresponde a las mayúsculas
       } else if (cifrar[i].charCodeAt() >= 65 && cifrar[i].charCodeAt() <= 90) {
-        let upper = (cifrar[i].charCodeAt()- 65 + offset) % 26 + 65; //26 letras del abecedario
+        let upper = (cifrar[i].charCodeAt() - 65 + offset) % 26 + 65; //26 letras del abecedario
         let codeNumber = String.fromCharCode(upper);                 //El String.fromCharCode() método estático que devuelve una cadena creada mediante el uso de una secuencia de valores Unicode especificada.
         cifrado += codeNumber;
         console.log("Mayúscula codificada");
@@ -48,19 +50,21 @@
         console.log("Inicio del descifrado")
 //Variables que nos ayudarán a descifrar el mensaje oculto
         let descifrado = "";
-        let codeNumber = "";
+        let codeNumber = ""; //Determina el código de algun carácter especial
 //iteraciones, descifrar.length determinará cuantas "vueltas" dará para encontrar el carácter correspondiente al descifrado de los carácteres ingresados
       for(let i=0; i<descifrar.length; i++) {
               console.log("El decodificado ha iniciado");
 //32 al 64 ASCII corresponde a los números y carácteres como "%&/())
       if (descifrar[i].charCodeAt() >= 33 && descifrar[i].charCodeAt() <= 64) {
-          codeNumber = (descifrar[i]);
+        let simbolo = (descifrar[i].charCodeAt()-64 - offset) % 32 + 64;
+        let codeNumber = String.fromCharCode(simbolo);
           descifrado += codeNumber;
           console.log(descifrado); //Muestra lo que hay en la variable en ese momento (relativo)
           console.log("Número y/o simbolo decodificado")
 //123 al 254 ASCII  corresponde a carácteres como letras con acentos y carácteres llaves, entre otras cosas, por ejemplo: ñ{á}éçó
       } else if (descifrar[i].charCodeAt() >= 123 && descifrar[i].charCodeAt() <= 254) {
-          codeNumber = (descifrar[i]);
+        let caracter = (descifrar[i].charCodeAt()- 254 - offset) % 132 + 254;
+        let codeNumber = String.fromCharCode(caracter);
           descifrado += codeNumber;
           console.log("Carácter Especial decodificado")
 //33 ASCII corresponde al "espacio"
